55bdfb8aee093bc9f1feac48883f7de48b8e8d6c
Ronald Magno <dlanoronald123@gmail.com>
2021-09-22 23:15:08 +0800

    Refactored components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,18 @@
-import React,{ useState } from 'react';
-import ExpenseDate from './ExpenseDate';
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    setTitle('Nigga');
+    setTitle("Nigga");
   };
-	
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date ={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">Php{props.amount}</div>
@@ -20,6 +20,6 @@ const ExpenseItem = (props) => {
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,9 +5,8 @@ import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
-    props.onAddExpense(expenseData)
+    props.onAddExpense(expenseData);
   };
- 
 
   return (
     <div className="new-expense">
