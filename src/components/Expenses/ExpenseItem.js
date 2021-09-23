49a9b945f2fe9748f3631e93c4b1d6c9550f8d05
Ronald Magno <dlanoronald123@gmail.com>
2021-09-23 20:43:28 +0800

    Rendering list of data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
-import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Nigga");
-  };
-
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">Php{props.amount}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-};
+}
 
 export default ExpenseItem;
